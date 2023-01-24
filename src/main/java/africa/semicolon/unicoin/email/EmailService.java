f338762cb36c6263956e3692f1138fe8d34d0314
bolaji <ibrahimkenny17@gmail.com>
2023-01-24 23:57:09 +0100

    added email service

--- src/main/java/africa/semicolon/unicoin/email/EmailService.java
+++ src/main/java/africa/semicolon/unicoin/email/EmailService.java
@@ -40,4 +40,28 @@ public class EmailService implements EmailSender {
         }
 
     }
+
+    @Override
+    public void sendEmail(String recipientEmail, String link) throws MessagingException {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+            helper.setFrom("kenny@gmail.com");
+            helper.setTo(recipientEmail);
+            String subject = "Here's the link to reset your password";
+
+            String content = "<p>Hello,</p>"
+                    + "<p>You have requested to reset your password.</p>"
+                    + "<p>Click the link below to change your password:</p>"
+                    + "<p><a href=\"" + link + "\">Change my password</a></p>"
+                    + "<br>"
+                    + "<p>Ignore this email if you do remember your password, "
+                    + "or you have not made the request.</p>";
+
+            helper.setSubject(subject);
+
+            helper.setText(content, true);
+
+            javaMailSender.send(message);
+        }
 }
+
